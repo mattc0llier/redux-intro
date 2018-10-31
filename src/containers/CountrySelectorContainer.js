1b6c44ea0d28aac49b3a930f29527691b8e5d0f0
mattc0llier <matt.collier@hotmail.com>
2018-10-31 16:03:03 +0000

    added change reduce with react-redux library

--- src/containers/CountrySelectorContainer.js
+++ src/containers/CountrySelectorContainer.js
@@ -1,71 +1,27 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import CountrySelector from '../components/CountrySelector';
 import { setSelectedCountry, setCountryListOpen } from '../actions';
 
-class CountrySelectorContainer extends React.Component {
-  constructor(props, context){
-    super(props, context);
 
-    const reduxState = this.context.store.getState();
-
-    this.state = {
-      selectedCountry: reduxState.country.selectedCountry,
-      countryListOpen: reduxState.country.countryListOpen
-    }
-
-    this.selectCountry = this.selectCountry.bind(this);
-    this.handleFocus = this.handleFocus.bind(this);
-    this.handleBlur = this.handleBlur.bind(this);
-  }
-
-  componentDidMount(){
-    const { store } = this.context;
-    store.subscribe( () => {
-      this.updateFromStore();
-    });
-  }
-
-  updateFromStore(){
-    const { store } = this.context;
-
-    const reduxState = store.getState();
-
-    this.setState({
-      selectedCountry: reduxState.country.selectedCountry,
-      countryListOpen: reduxState.country.countryListOpen
-    });
-  }
-
-  selectCountry(country){
-    this.context.store.dispatch(setSelectedCountry(country));
-
-    this.context.store.dispatch(setCountryListOpen(false));
-  }
-
-  handleFocus(event){
-    this.context.store.dispatch(setCountryListOpen(true));
-  }
-
-  handleBlur(event){
-    this.context.store.dispatch(setCountryListOpen(false));
-  }
-
-  render(){
-    return (
-      <CountrySelector
-        selectedCountry={this.state.selectedCountry}
-        countryListOpen={this.state.countryListOpen}
-        selectCountry={this.selectCountry}
-        handleFocus={this.handleFocus}
-        handleBlur={this.handleBlur}
-      />
-    );
+const mapStateToProps = state => {
+  return {
+    selectedCountry: state.country.selectedCountry,
+    countryListOpen: state.country.countryListOpen
   }
 }
 
-CountrySelectorContainer.contextTypes = {
-  store: PropTypes.object
+const mapDispatchToProps = dispatch =>{
+  return{
+    handleFocus: event => dispatch(setCountryListOpen(true));
+    handleBlur: event => dispatch(setCountryListOpen(false));
+    selectCountry: country => {
+      dispatch(setSelectedCountry(country));
+      dispatch(setCountryListOpen(false));
+    }
+  }
 };
 
-export default CountrySelectorContainer;
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps)
+  (CountrySelector);
